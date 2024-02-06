cf49a913d11685f3eac7842f917763a60ecea942
IhorSherstiuk <mycat201198@gmail.com>
2024-02-05 17:26:20 -0800

    chore: remove api key

--- crawlers/tmdb_crawler.py
+++ crawlers/tmdb_crawler.py
@@ -1,19 +1,20 @@
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
 import json
-
+import os
+from dotenv import load_dotenv
 
 def main():
   tmdb = TMDb()
-  tmdb.api_key = '536ca59623dab55deb3d16bc474ea2e0'
+  load_dotenv()
+  tmdb.api_key = os.getenv('TMDB_API_KEY')
   tmdb.language = 'en'
   tmdb.debug = True
   movie = Movie()
 
-
   popular = movie.popular()
   
-  keys_save = ('adult', 'backdrop_path', 'belongs_to_collection', 'budget', 'genres', 'id', 'imdb_id', 'original_language', 'original_title', 'overview', 'popularity', 'poster_path', 'production_countries', 'release_date', 'revenue', 'runtime', 'spoken_languages', 'status', 'tagline', 'title', 'video', 'vote_average', 'vote_count', 'casts',  'keywords')
+  keys_save = ('adult', 'belongs_to_collection', 'budget', 'genres', 'id', 'imdb_id', 'original_language', 'original_title', 'overview', 'popularity', 'poster_path', 'production_countries', 'release_date', 'revenue', 'runtime', 'spoken_languages', 'status', 'tagline', 'title', 'vote_average', 'vote_count', 'casts',  'keywords')
 
   with open("tmdb_sample.json", "w") as outfile:
     outfile.write('[')
@@ -21,8 +22,8 @@ def main():
     for item in popular:
       details = movie.details(item.id)
       dict = {}
-      for key in keys_save:
-        if (key not in details._json.keys()):
+      for key in details._json.keys():
+        if (key not in keys_save):
           continue
         
         dict[key] = details._json[key]
