17468a18fe8a2ab73d6e7e647e1c2117f07f5c0c
IhorSherstiuk <mycat201198@gmail.com>
2024-02-05 20:08:26 -0800

    chore: create sample data for genres

--- createDB/createSample.js
+++ createDB/createSample.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
 db = db.getSiblingDB('sampledb')
+
+// Populate the database with sample movies
 db.createCollection('movies')
 moviesCollection = db.getCollection("movies")
 moviesCollection.remove({})
@@ -8,3 +10,12 @@ moviesCollection.remove({})
 const data = JSON.parse(fs.readFileSync('tmdb_sample.json', 'utf8'));
 
 moviesCollection.insertMany(data);
+
+// Populate the database with sample genres
+db.createCollection('movieGenres')
+genresCollection = db.getCollection("movieGenres")
+genresCollection.remove({})
+
+const genres = JSON.parse(fs.readFileSync('tmdb_sample_genres.json', 'utf8'));
+
+genresCollection.insertMany(genres);
