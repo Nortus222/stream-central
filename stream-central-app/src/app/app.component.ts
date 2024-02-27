52a253a9ff3dd8dc59382f3b33f08f61975a340d
David Stanko <71538514+David-S-2002@users.noreply.github.com>
2024-02-26 22:08:13 -0800

    Started 2nd component but have bugs

--- stream-central-app/src/app/app.component.ts
+++ stream-central-app/src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, HomePageComponent],
   templateUrl: './app.component.html',
   template: `<h1>Testing!</h1>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'homes'; // component title
+  title = 'Stream Central'; // component title
 }
 
 // Next component: individual content pages
