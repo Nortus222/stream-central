e9b700cb7e1534d574e2ef27a233e5957de970f9
David Stanko <71538514+David-S-2002@users.noreply.github.com>
2024-02-26 21:32:13 -0800

    Nav bar Angular component done

---
+++ stream-central-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavBarComponent],
+  templateUrl: './app.component.html',
+  template: `<h1>Testing!</h1>`,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homes'; // component title
+}
+
+// Next component: individual content pages
+// They had the user story under each of the tasks in a certain theme
+// Depict each task under a certain user story
