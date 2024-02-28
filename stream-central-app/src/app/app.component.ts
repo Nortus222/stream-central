86cefa815113bbb2627ded8a6275d1dcf63e47cb
David Stanko <71538514+David-S-2002@users.noreply.github.com>
2024-02-27 16:17:28 -0800

    Nav bar component

--- stream-central-app/src/app/app.component.ts
+++ stream-central-app/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { HomePageComponent } from './home-page/home-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, HomePageComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   template: `<h1>Testing!</h1>`,
   styleUrl: './app.component.css'
