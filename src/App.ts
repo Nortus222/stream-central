c6fa1003ebd47b8abc9573bce63802d63d43132f
IhorSherstiuk <mycat201198@gmail.com>
2024-03-12 14:36:32 -0700

    try again

--- src/App.ts
+++ src/App.ts
@@ -68,7 +68,7 @@ class App {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "*");
-    res.redirect('/auth/google');
+    res.redirect('#/auth/google');
   }
 
   // Configure API endpoints.
