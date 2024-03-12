589d02d2a97d5a3c27a2ec67c42bc8b44e2def3c
Erkhembayar (Erik) Tsogtbaatar <148504714+erik-tsogt@users.noreply.github.com>
2024-03-11 22:55:04 -0700

    chore: minor fixes

--- src/googleOAuth2.ts
+++ src/googleOAuth2.ts
@@ -1,10 +1,8 @@
-//import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
-//dotenv.config();
+dotenv.config();
 
-//export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-//export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 
-export const GOOGLE_CLIENT_ID='321343580149-7msslemavpks4dpoa7cgt1ku34bi6gob.apps.googleusercontent.com'
-export const GOOGLE_CLIENT_SECRET='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'
